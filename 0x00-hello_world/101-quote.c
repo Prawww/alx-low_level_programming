3fea300ea8a002ccb0e2f3433670671aa60e28ec
anamkara <philipnyoro4@gmail.com>
2022-09-08 13:48:50 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: 1
- */
+* main  -entry point
+*
+* Return: 1
+*/
 int main(void)
 {
 	fprintf(stderr, "%s", "and that piece of art is useful\"");
