9e82d4ca25252ca983048a741e208c40a435fa01
anamkara <philipnyoro4@gmail.com>
2022-09-08 12:37:13 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#!/bin/bash
 #include<stdio.h>
 
+/*
+ * main- entry point
+ *
+ * using puts
+ */
 int main ()
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 
 }
