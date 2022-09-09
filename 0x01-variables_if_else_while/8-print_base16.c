f5bfa1b7783cac1a5ffe0b95c8dbcbfff071e4ff
anamkara <philipnyoro4@gmail.com>
2022-09-09 14:30:46 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Description: hexadecimal numbers
+ *
+ * return 0 (success)
+ */
+int main(void)
+{
+	int num;
+	char c;
+
+	for (num = 48; num <= 57; num++)
+		putchar((char) num);
+	for (c = a; c <= f; c++)
+		putchar(c);
+	 
+	putchar('\n');
