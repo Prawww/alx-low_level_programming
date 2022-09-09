c92e59d8d016aaf9508753c39835767b1f43b001
anamkara <philipnyoro4@gmail.com>
2022-09-09 14:18:26 -0700

    print in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Description: integer values
+ *
+ * return 0 (sucess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
