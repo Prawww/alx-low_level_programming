e85fb01b60a7803910fa2ad4db9f01b64f111324
anamkara <philipnyoro4@gmail.com>
2022-09-09 13:43:43 -0700

    print the natural numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Description: integer values
+ *
+ * return 0 (sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
