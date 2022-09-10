b1147f393215d0bbc9118c175b6c3cec4885d584
anamkara <philipnyoro4@gmail.com>
2022-09-10 02:41:36 -0700

    combi 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,19 +8,23 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int num1, num2, num3;
 
 	for (num1 = 48; num1 <= 56; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 57; num2++)
 		{
-			putchar((char)num1);
-			putchar((char)num2);
-		       	if(num1 != 56)
+			for(num3 = num2++; num3 <= 58; num3++)
 			{
-		       		putchar(',');
-			       	putchar(' ');
-		       }
+				putchar((char)num1);
+				putchar((char)num2);
+				putchar((char)num3);
+		       		if(num1 != 56)
+				{
+		       			putchar(',');
+			       		putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
