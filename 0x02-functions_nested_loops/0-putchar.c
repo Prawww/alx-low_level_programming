900d2b9a6e5db786eeedf9b77135f3e8e985effc
anamkara <philipnyoro4@gmail.com>
2022-09-13 13:56:15 -0700

    using header files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "main.h"
 
 int main(void)
 {
@@ -10,5 +10,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n')
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-ifndef _HEADER_H_
-#define _HEADER_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 #endif
