1976cd8d1aa5628001f3640327737fc7428cc9f0
anamkara <philipnyoro4@gmail.com>
2022-09-14 10:53:36 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_to_98 - print a times table for a given number n
+* @n: the starting number
+*/
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d", n);
+		if (n > 98)
+			--n;
+		else if (n < 98)
+			++n;
+		printf(", ");
+	}
+	printf("%d", 98);
+	printf("\n");
+}
