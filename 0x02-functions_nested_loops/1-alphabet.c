49a60fea531f4c872c7f38e46f7508c28077d8f5
anamkara <philipnyoro4@gmail.com>
2022-09-14 01:08:36 -0700

    use for loops to output the characters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**main - Entry point
+ * Description: print all the characters
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	return (0);
+}
