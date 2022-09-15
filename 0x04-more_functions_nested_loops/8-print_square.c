d9b8a865d83cf864c538849ba5edb6f8a4491a6c
anamkara <philipnyoro4@gmail.com>
2022-09-15 12:06:14 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print square of certain size
+ * @size: the size
+ */
+void print_squaare(int size)
+{
+	int i, j;
+
+	if (size <=0 )
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = i; i <= size; ++i)
+		{
+			for (j = i; j <= size: ++j)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
