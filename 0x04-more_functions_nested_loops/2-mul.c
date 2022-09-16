a1f2cba91996b2cd74d57992db5bb8f8a34828c7
anamkara <philipnyoro4@gmail.com>
2022-09-16 12:57:29 -0700

    mulplication function

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * mul - to add two inetgers
+ * mul - to add two integers
+ * @a: first variable
+ * @b: second variable
  * Return:0 (success)
  */
 int mul(int a, int b)
