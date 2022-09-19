4966d33db9f206684a6f417b598e409f8cbf731d
anamkara <philipnyoro4@gmail.com>
2022-09-19 03:27:54 -0700

    switch values

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 
 /**
  * reset_to_98 - reassign variable n
+ * @n - check if it has become 98
  */
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**\
+ * swap_int - swap values of an integer
+ * @a - has it changed to the value of b
+ * @b - has it changed to the value of a
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
