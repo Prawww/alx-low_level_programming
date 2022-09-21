5abb94a854bd63751ad2923e91ce761755101d99
anamkara <philipnyoro4@gmail.com>
2022-09-21 12:38:50 -0700

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,10 @@
  */
 int _strcmp(char *s1, char *src)
 {
-	int i = 0;
+	int i;
 	int R;
+
+	i=0;
 
 	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))	
 	{
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 #endif
