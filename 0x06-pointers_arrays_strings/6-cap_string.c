377c2382294204e06ffaf8fbc56bcbab5f30d1ed
anamkara <philipnyoro4@gmail.com>
2022-09-21 22:39:48 -0700

    prints the buffer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,18 +6,18 @@
  * @s: pointer
  * Return: char 
  */
-char *cap_string(char * s)
-{ 
+char *cap_string(char *s)
+{
 	int i, j;
-	char del[] = " \t\n,;!?\".(){}";
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[0] >= 97 && s[0] <= 122)
 			s[0] = s[0] - 32;
-				for (j = 0; del[j] != '\0'; j++)
-					if (s[i] == del[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-						s[i + 1] + s[i + 1] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
