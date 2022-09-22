443f568342bb28a64907169246e34df4077192eb
anamkara <philipnyoro4@gmail.com>
2022-09-21 23:07:38 -0700

    print the numbers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * *string_to upper - to upper
- * Return: 0 (success)
+ * string_toupper - change all lowercase letters in a string to uppercase
+ * @s: the string to processed
+ * Return: the resultant string
  */
 char *string_toupper(char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
