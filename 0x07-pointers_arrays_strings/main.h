67a125da02cd4ed99f390fb4ca48f942f1fa063b
anamkara <philipnyoro4@gmail.com>
2022-09-26 21:29:21 -0700

    to get length of prefix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 char *_memset(char *str, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 #endif
