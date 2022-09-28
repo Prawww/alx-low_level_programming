ec4b6aa21683142940b8f88f7c04d98703d53e07
anamkara <philipnyoro4@gmail.com>
2022-09-28 14:13:28 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,13 +9,13 @@
 int is_palindrome(char *s)
 {
 	int l = 0;
-	int h = strlen(*s) - 1;
+	int h = strlen(s) - 1;
 
 	while (h > l) {
-		if (*s[l++] != *s[h--]) {
-			printf("%s is not a palindrome\n", *s);
+		if (s[l++] != s[h--]) {
+			printf("%s is not a palindrome\n", s);
 		}
 	}
-	printf("%s is a palindrome\n", *s);
+	printf("%s is a palindrome\n", s);
 	return (0);
 }
