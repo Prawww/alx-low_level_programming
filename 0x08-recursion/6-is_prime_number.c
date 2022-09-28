2948a55adf47cb4b1f67a24ecc2368a46119041d
anamkara <philipnyoro4@gmail.com>
2022-09-28 13:50:39 -0700

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * check_prime - check whether a number is primt
+ * @num: the number to check
+ * @i: the number to divide @num 
+ * Return: 0 (success)
+ */
+int check_prime(int num, int i)
+{
+	if (i * i > num)
+		return (1);
+	else if (num % i == 0)
+		return (0);
+	return (check_prime(num, i + 1));
+}
+/**
+ * is_prime_number - check whether prime
+ * @num: the number to check
+ * Return: 0 (success)
+ */
+int is_prime_number(int num)
+{
+	if (num <= 1)
+		return (0);
+	return (check_prime(num,2));
+}
