ecf5d3c823dcfbae817c4956f093bd885b2152be
anamkara <philipnyoro4@gmail.com>
2022-10-03 10:17:54 -0700

    main arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all the arguments
  * @argc: number of arguments
- * argv: array of arguments
+ * @argv: array of arguments
  * Return: 0 (success)
  */
 int main(int argc, char *argv[])
