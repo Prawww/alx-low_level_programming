3c6b2aa79d4c5d3c86dbd347a9ecd9327cea27b4
anamkara <philipnyoro4@gmail.com>
2022-10-03 09:53:49 -0700

    main arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,11 +6,11 @@
  * argv: array of arguments
  * Return: 0 (success)
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
-	while (i++ < argc)
+	for (i = 0; i < argc; ++i)
 		printf("%s\n", argv[i]);
 	return(0);
 }
