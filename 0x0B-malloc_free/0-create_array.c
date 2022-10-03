ecf79999ed933c2ee1e63bb5ad27f3ed44a5b45d
anamkara <philipnyoro4@gmail.com>
2022-10-03 12:33:03 -0700

    memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 
 /**
  * create_array - creates an array of chars
- * @size: the size 
+ * @size: the size
  * @c: the character
  * Return: 0 (success)
  */
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = malloc(size *sizeof(char));
+	ptr = malloc(size * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	while (i < size)
