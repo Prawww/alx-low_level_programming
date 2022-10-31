a84744cd0a9503df18f0f5efe38e87e972119464
anamkara <philipnyoro4@gmail.com>
2022-10-31 05:11:46 -0700

    more dingly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -37,3 +37,15 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	return (-1);
 }
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * free_listint2 - frees a linked list
