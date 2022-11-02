b67f0a4e96d615d07297da5e445f73c61ce840d9
anamkara <philipnyoro4@gmail.com>
2022-11-02 13:29:39 -0700

    file descriptions

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: command line arguments
+ * Description: copies content of a file to another file
+ * Return: 0 success
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+	long read_count, wirte_count;
+	char buffer[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((read_count = read(fd_from, buffer, 1024)) != 0)
+	{
+		if (read_count == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		write_count = write(fd_to, buffer, read_count);
+		if (write_count == -1 || (write_count != read_count))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+	return (0);
+}
