c7bd6e5ab2461dc6d72e15cbe94047b0f78f7a3f
anamkara <philipnyoro4@gmail.com>
2022-11-09 00:51:54 -0800

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,10 +30,7 @@ int main(int argc, char *argv[])
 
 		}
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	mul = num1 * num2;
+	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%lu\n", mul);
 	return (0);
 }
