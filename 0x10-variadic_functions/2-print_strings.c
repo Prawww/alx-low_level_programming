3ea5f491b74f27e47e628d587dd40f42897e2b47
anamkara <philipnyoro4@gmail.com>
2022-11-23 05:35:34 -0800

    variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,7 +4,7 @@
  * print_strings - prints strings
  *@separator: separator
  *@n: number of strings
- * Return: nothing 
+ * Return: nothing
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
