1f1e02775052217805e3deaa06d2b1e8dac99305
anamkara <philipnyoro4@gmail.com>
2022-12-08 08:43:25 -0800

    doubly linked lists

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint linked list
+ * @head: double pointer to the head
+ * @index: index of the node 
+ * Return: if the node does not exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i;
+
+	current = head;
+	i = 0;
+	while (current != NULL)
+	{
+		if (i == index)
+			return (current);
+		current = current->next;
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum all the data(n) of a dlistint_t linked list
+ * @head: double pointer to the head of the list
+ * Return: if the list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *i;
+
+	for (i = head; i != NULL; i = i->next)
+	{
+		sum += i->n;
+	}
+	return (sum);
+}
